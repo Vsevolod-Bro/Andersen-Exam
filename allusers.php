f76b717df1b4f95f6ffd7ae59c7c7be73c718ab5
Vsevolod-Bro <brvv.job@gmail.com>
2021-04-12 17:39:50 +0700

    ready app2

--- allusers.php
+++ allusers.php
@@ -4,11 +4,13 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
 <link rel="stylesheet" type="text/css" href="style.css">
-<title>Selected User</title>
+<title>App2 - Show all users</title>
 </head>
 <body>
+<h1>PHP Application</h1>
+<h1>view all user from DB</h1>
 <?php
-#include("/var/www/html/base.php");
+
 $dsn = 'mysql:host=172.31.100.87;port=3306;dbname=brvv';
 $db_name = 'brvv';
 $db_username =  'root';
@@ -19,11 +21,14 @@ $dbh = new PDO($dsn, $db_username, $db_pass);
 $sql_select = "SELECT * FROM users";
 $result = $dbh->query($sql_select);
 $row = $result->fetch();
+printf("");
 do
 {
+        printf("__________________________________________________________");
         printf("<p>User: " .$row['fst_name'] . " " .$row['lst_name'] ."</p>
-        <p><i>Contacts</i></p><p>phone: " .$row['phone'] . "</p>---------<br/>"
+        <p><i>Contacts</i></p><p>phone: " .$row['phone'] . "</p>"
         );
+        printf("__________________________________________________________");
 }
 while($row = $result->fetch());
          ?>
--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+
 <link rel="stylesheet" type="text/css" href="style.css">
-<title>Data Output</title>
+<title>Andersen Exam Application 2</title>
 </head>
 
 <body>
+<section class="congratulations">
+        <h1>Hello World 2</h1>
+        <h1>PHP Application</h1>
+        <p>It is the <em>PHP</em> Application running at AWS Elastic Beanstalk in AWS Cloud</p>
+        <p>Application use MySQL DB connection to the DB on EC2 instance in this VPC </p>
+        <p>Press the button to get some test data form the remote DB</p>
+    </section>
 <fieldset>
 <form method="post" action="allusers.php">
 <input id="submit" type="submit" value="Show all users"><br/>
